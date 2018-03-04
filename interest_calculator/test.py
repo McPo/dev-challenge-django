282380cf0ef11b101c654b98f3afd5e3f8a1d86b
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-03-04 16:41:01 +0000

    Updated test to check end value

--- interest_calculator/test.py
+++ interest_calculator/test.py
@@ -5,10 +5,21 @@ class InterestCalculatorTestCase(TestCase):
     def setUp(self):
         pass
 
+    # Results tested against https://www.thecalculatorsite.com/finance/calculators/compoundinterestcalculator.php#compoundinterval
     def test_calculate(self):
         c = Client()
-        response = c.post('/calculate/', json.dumps({'currentBalance': 0, 'monthlyDeposit': 0, 'interestRate': 0, 'compoundPeriod': 'monthly'}), content_type="application/json")
+        request = {
+            'currentBalance': 10,
+            'monthlyDeposit': 100,
+            'interestRate': 1,
+            'compoundPeriod': 'monthly'
+        }
+        response = c.post('/calculate/', json.dumps(request), content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
         json_response = json.loads(response.content)
-        self.assertEqual(len(json_response['monthly_balance']), 600)
+        monthly_balance = json_response['monthly_balance']
+
+        self.assertEqual(len(monthly_balance), 600)
+        self.assertEqual(monthly_balance[0], 110.09)
+        self.assertEqual(monthly_balance[-1], 77,886.68)

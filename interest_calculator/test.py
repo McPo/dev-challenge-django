349dcf03ded849893b8760c8607cec96c83b0712
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-03-04 16:24:55 +0000

    Added very basic django test

---
+++ interest_calculator/test.py
@@ -0,0 +1,14 @@
+import json
+from django.test import TestCase, Client
+
+class InterestCalculatorTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_calculate(self):
+        c = Client()
+        response = c.post('/calculate/', json.dumps({'currentBalance': 0, 'monthlyDeposit': 0, 'interestRate': 0}), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content)
+        self.assertEqual(len(json_response['monthly_balance']), 600)

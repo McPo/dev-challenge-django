7e0a026d7e67d247dd90a9ea4a5f67d0cc830c1f
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-03-04 16:49:50 +0000

    Added yearly test

--- interest_calculator/test.py
+++ interest_calculator/test.py
@@ -6,7 +6,7 @@ class InterestCalculatorTestCase(TestCase):
         pass
 
     # Results tested against https://www.thecalculatorsite.com/finance/calculators/compoundinterestcalculator.php#compoundinterval
-    def test_calculate(self):
+    def test_calculate_monthly(self):
         c = Client()
         request = {
             'currentBalance': 10,
@@ -22,4 +22,22 @@ class InterestCalculatorTestCase(TestCase):
 
         self.assertEqual(len(monthly_balance), 600)
         self.assertEqual(monthly_balance[0], 110.09)
-        self.assertEqual(monthly_balance[-1], 77,886.68)
+        self.assertEqual(monthly_balance[-1], 77886.68)
+
+    def test_calculate_yearly(self):
+        c = Client()
+        request = {
+            'currentBalance': 10,
+            'monthlyDeposit': 100,
+            'interestRate': 1,
+            'compoundPeriod': 'yearly'
+        }
+        response = c.post('/calculate/', json.dumps(request), content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+        json_response = json.loads(response.content)
+        monthly_balance = json_response['monthly_balance']
+
+        self.assertEqual(len(monthly_balance), 600)
+        self.assertEqual(monthly_balance[0], 110)
+        self.assertEqual(monthly_balance[-1], 77790.64)

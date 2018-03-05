95255df78fc5f16e648d3fd2d3789975b696972f
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-03-05 19:45:21 +0000

    Added bootstrap made it look a bit better. Trying to make it good with materialize was a mistake

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,22 +2,86 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/App.css';
 
-const App = props => <div className="App">
-    <header className="App-header">
-        <h1 className="App-title">Finimize dev challenge</h1>
-    </header>
-    <form>
-        <input placeholder="Initial Account Balance" type="number" min="0" defaultValue={ props.currentBalance } onChange={ props.onCurrentBalanceChange } />
-        <input placeholder="Monthly Deposits" type="number" min="0" defaultValue={ props.monthlyDeposit } onChange={ props.onMonthlyDepositChange } />
-        <input placeholder="Interest Rate" type="number" min="0" defaultValue={ props.interestRate } onChange={ props.onInterestRateChange } />
-        <select  defaultValue={ props.compoundPeriod } onChange={ props.onCompoundPeriodChange } >
-            <option value="monthly">Month</option>
-            <option value="yearly">Year</option>
-        </select>
-    </form>
-    <ol>
-        { props.futureMonthlyBalance.map((b, i) => <li key={ i }>{ b }</li>) }
-    </ol>
+const App = props => <div className="container">
+    <div className="row">
+        <div className="col-sm">
+            <header className="page-header">
+                <h1 className="display-1">Finimize</h1>
+            </header>
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-sm">
+            <div className="card">
+                <div className="card-header">Input</div>
+                <div className="card-body">
+                    <form>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-sm">
+                                    <div className="form-group">
+                                        <label htmlFor="current-balace">Initial Account Balance</label>
+                                        <input type="number" min="0" defaultValue={ props.currentBalance } onChange={ props.onCurrentBalanceChange } id="current-balace" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="col-sm">
+                                    <div className="form-group">
+                                        <label htmlFor="monthly-deposit">Monthly Deposits</label>
+                                        <input type="number" min="0" defaultValue={ props.monthlyDeposit } onChange={ props.onMonthlyDepositChange } id="monthly-deposit" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="col-sm">
+                                    <div className="form-group">
+                                        <label htmlFor="interest-rate">Interest Rate</label>
+                                        <input type="number" min="0" defaultValue={ props.interestRate } onChange={ props.onInterestRateChange } id="interest-rate" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="col-sm">
+                                    <div className="form-group">
+                                        <label htmlFor="compound-period">Compound Period</label>
+                                        <select  defaultValue={ props.compoundPeriod } onChange={ props.onCompoundPeriodChange } id="compound-period" className="custom-select" >
+                                            <option value="monthly">Month</option>
+                                            <option value="yearly">Year</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="row">
+        <div className="col-sm">
+            <div className="card bg-dark text-white">
+                <div className="card-header">Results</div>
+                <div className="card-body">
+                    {
+                        props.futureMonthlyBalance.length ?
+                            <table className="table table-striped table-dark">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Balance</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        props.futureMonthlyBalance.map((b, i) => (
+                                            <tr>
+                                                <th scope="row">{ i + 1 }</th>
+                                                <td>{ b }</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table> : <div className="alert alert-dark" role="alert">No Results</div>
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
 </div>;
 
 App.propTypes = {
